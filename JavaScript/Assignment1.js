72980dac71ed99a2207cb96e1f7e37a4754b79da
henry-codes007 <benedicthenry724@gmail.com>
2024-09-03 13:27:29 +0100

    done

---
+++ JavaScript/Assignment1.js
@@ -0,0 +1,17 @@
+let randomNumbers = [];
+
+for(let i = 1; i <= 20; i++){
+
+    // Generate a random number between 0 and 0.9
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+
+    //Multiply then by 30 to make them greater than 1
+    randomNumber = randomNumber *30;
+    console.log(randomNumber);
+
+    //Convert them to integers              
+    randomNumber = Math.randomNumber
+
+
+}
